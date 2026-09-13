dde160a72c491777f1d1c4378e24a6f6aac0c3d4
hritikjena <hritikjn6@gmail.com>
2026-09-14 02:57:23 +0530

    fix: update stockfish download URL to universal linux build

--- render-build.sh
+++ render-build.sh
@@ -7,9 +7,9 @@ pip install -r requirements.txt
 # Download stockfish if not exists
 if [ ! -f "stockfish-ubuntu" ]; then
     echo "Downloading Stockfish..."
-    curl -L -o stockfish.tar.gz https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64-modern.tar
+    curl -L -o stockfish.tar.gz https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-linux-x86-64-universal.tar.gz
     tar -xvf stockfish.tar.gz
-    mv stockfish/stockfish-ubuntu-x86-64-modern ./stockfish-ubuntu
+    mv stockfish/stockfish-linux-x86-64-universal ./stockfish-ubuntu
     chmod +x ./stockfish-ubuntu
     rm -rf stockfish stockfish.tar.gz
 fi
